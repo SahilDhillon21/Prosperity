86a84dd5582b691a4c8c5058920a98fefe404d18
SahilDhillon21 <sodhillon_b21@it.vjti.ac.in>
2023-09-26 21:29:13 +0530

    added note model and routes

---
+++ backend/src/controllers/note.controller.ts
@@ -0,0 +1,9 @@
+export const getNotes = async () => {
+    // try {
+        
+    // } catch (error) {
+    //     alert(error)
+    //     alert("Couldn't fetch notes (error in not controller 'getNotes')")
+    // }
+}
+
