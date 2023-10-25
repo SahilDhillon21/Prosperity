afc582788720d897fb0efaed084d6b30939c3be8
SahilDhillon21 <sodhillon_b21@it.vjti.ac.in>
2023-10-25 14:51:49 +0530

    habit paper card added

--- frontend/src/components/Habits.tsx
+++ frontend/src/components/Habits.tsx
@@ -1,9 +1,11 @@
 import { Button, CssBaseline } from "@mui/material";
 import { useEffect, useState } from "react";
-import { Container, Offcanvas, Stack } from "react-bootstrap";
+import { Col, Container, Offcanvas, Row, Stack } from "react-bootstrap";
 import Habit from "../models/habit.model";
 import { CreateEditHabit } from "./CreateEditHabit";
-import * as HabitNetwork from '../network/habit.network' 
+import * as HabitNetwork from '../network/habit.network'
+import Paper from '@mui/material/Paper';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 
 
 
@@ -11,7 +13,7 @@ export const Habits = () => {
 
   const [habits, setHabits] = useState<Habit[]>([])
 
-  const [habitToEdit, setHabitToEdit] = useState<Habit|null>(null)  
+  const [habitToEdit, setHabitToEdit] = useState<Habit | null>(null)
 
   useEffect(() => {
 
@@ -26,7 +28,7 @@ export const Habits = () => {
 
     getHabits()
 
-  }, [])
+  }, [habits])
 
   const [showCreateEditHabitModal, setShowCreateEditHabitModal] = useState(false)
 
@@ -37,19 +39,33 @@ export const Habits = () => {
 
         <Container className="mt-3">
 
-          {habits.map((habit => {
-            return (
-              <h1>{habit.name}</h1>
-            )
-          }))}
+          <Row className="justify-content-center d-flex">
+            <Col xs={3} md={3} >
+              <Paper elevation={3} className="px-3 py-2">
+                <Row>
+                  <Col xs={9} md={9} lg={9}>
+                    {habits.map((habit => {
+                      return (
+                        <h1>{habit.name}</h1>
+                      )
+                    }))}
+                  </Col>
+
+                  <Col xs={3} md={3} lg={3} className="mt-2 mb-0">
+                    <CheckBoxOutlineBlankIcon fontSize="large" style={{cursor: "pointer"}}/>
+                  </Col>
+                </Row>
+              </Paper>
+            </Col>
+          </Row>
 
           <Button onClick={() => setShowCreateEditHabitModal(true)}> + Add </Button>
 
           {showCreateEditHabitModal &&
-            <CreateEditHabit 
-            habitToEdit={habitToEdit} 
-            onDismiss={() => setShowCreateEditHabitModal(false)}
-            onHabitSaved={() => {}}
+            <CreateEditHabit
+              habitToEdit={habitToEdit}
+              onDismiss={() => setShowCreateEditHabitModal(false)}
+              onHabitSaved={() => { }}
             />
           }
 
